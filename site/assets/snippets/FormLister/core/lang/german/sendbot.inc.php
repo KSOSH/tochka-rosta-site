d612492a7b8a0d399845df5e6e322de9ebcb0aff
ProjectSoft <projectsoft2009@yandex.ru>
2023-06-11 20:09:48 +0400

    add lexicon

---
+++ site/assets/snippets/FormLister/core/lang/german/sendbot.inc.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by SublimeText.
+ * User: ProjectSoft
+ * Date: 11.06.2023
+ * Time: 20:01:55
+ */
+
+setlocale(LC_ALL, 'ru_RU.UTF-8');
+
+$_lang = array();
+$_lang['sendbot.send_failed'] = 'Не удалось отправить данные.';
+$_lang['sendbot.send_success'] = 'Данные успешно отправлены.';
+
+return $_lang;
